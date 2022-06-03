332b6761a0fa72edb00fa932ed235503840e5459
gozzafadillah <74459029+gozzafadillah@users.noreply.github.com>
2022-06-03 07:37:24 +0700

    Refactor: add logic and fix bugs

--- users/service/usecase.go
+++ users/service/usecase.go
@@ -2,7 +2,6 @@ package service_users
 
 import (
 	"errors"
-	"fmt"
 	"ppob/app/middlewares"
 	domain_users "ppob/users/domain"
 )
@@ -12,19 +11,6 @@ type UsersService struct {
 	jwtauth    *middlewares.ConfigJwt
 }
 
-// Register implements domain_users.Service
-func (us UsersService) Register(domain domain_users.Users) (domain_users.Users, error) {
-	id, err := us.Repository.Store(domain)
-	if err != nil {
-		return domain_users.Users{}, errors.New("gagal di tambahkan")
-	}
-	data, err := us.Repository.GetById(id)
-	if err != nil {
-		return domain_users.Users{}, errors.New("gagal di tambahkan")
-	}
-	return data, nil
-}
-
 func NewUsersService(repo domain_users.Repository, jwt *middlewares.ConfigJwt) domain_users.Service {
 	return UsersService{
 		Repository: repo,
@@ -32,10 +18,22 @@ func NewUsersService(repo domain_users.Repository, jwt *middlewares.ConfigJwt) d
 	}
 }
 
+// Register implements domain_users.Service
+func (us UsersService) Register(domain domain_users.Users) (domain_users.Users, error) {
+	id, err := us.Repository.Store(domain)
+	if err != nil {
+		return domain_users.Users{}, errors.New("gagal ditambahkan")
+	}
+	data, err := us.Repository.GetById(id)
+	if err != nil {
+		return domain_users.Users{}, errors.New("gagal ditambahkan")
+	}
+	return data, nil
+}
+
 // Login implements domain_users.Service
 func (us UsersService) Login(email string, password string) (string, error) {
 	data, err := us.Repository.CheckEmailPassword(email, password)
-	fmt.Println("data login : ", data)
 	if err != nil {
 		return "", errors.New("failed to generate token, user not found")
 	}
