7fd0046d59af30b370d1f5fd3f3215f63bb53bb0
gozzafadillah <74459029+gozzafadillah@users.noreply.github.com>
2022-06-10 19:48:13 +0700

    fix: add cors

--- routes/route.go
+++ routes/route.go
@@ -16,11 +16,25 @@ type ControllerList struct {
 func (cl *ControllerList) RouteRegister(e *echo.Echo) {
 
 	// product public
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"server-front-end"},
+		AllowMethods:     []string{"GET", "POST", "OPTIONS", "PUT", "DELETE"},
+		AllowCredentials: true,
+		MaxAge:           2592000,
+	}))
+	// product public
 	e.POST("/login", cl.UserHandler.Authorization)
 	e.POST("/register", cl.UserHandler.Register)
 	e.GET("/admin/users", cl.UserHandler.GetUsers)
 	e.GET("/admin/user/:phone", cl.UserHandler.GetUserForAdmin)
+	// product public
 	authUser := e.Group("user")
+	authUser.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"server-front-end"},
+		AllowMethods:     []string{"GET", "POST", "OPTIONS", "PUT", "DELETE"},
+		AllowCredentials: true,
+		MaxAge:           2592000,
+	}))
 	authUser.Use(middleware.JWTWithConfig(cl.JWTMiddleware), valid.RoleValidation("customer", cl.UserHandler))
 	authUser.POST("/account", cl.UserHandler.InsertAccount)
 	authUser.GET("/profile", cl.UserHandler.GetUserForCustomer)
