5dae794a764850efcba0adba589f46312668c3b4
gozzafadillah <74459029+gozzafadillah@users.noreply.github.com>
2022-06-25 08:21:08 +0700

    add core domain transaction

--- transaction/domain/core.go
+++ transaction/domain/core.go
@@ -1,1 +1,27 @@
 package domain_transaction
+
+import (
+	"time"
+)
+
+type Transaction struct {
+	ID               int
+	Transaction_Code string
+	ID_Pelanggan     string
+	Phone            string
+	Amount           int
+	Payment_Id       string
+	Status           string
+}
+
+type Payment struct {
+	Payment_Id string
+	Method     string
+	Channel    string
+	Paid_at    time.Time
+}
+
+type Detail_Transaction struct {
+	Product_Detail_code string
+	Transaction_Code    string
+}
